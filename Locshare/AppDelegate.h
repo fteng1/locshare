a84eecca7c7b5aea31ecb0499ac0f693c5e31f77
Felianne Teng <felianneteng@fb.com>
2021-07-12 09:47:44 -0700

    Initial Commit

---
+++ Locshare/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Locshare
+//
+//  Created by Felianne Teng on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
