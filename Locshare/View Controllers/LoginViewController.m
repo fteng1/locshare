58c15ff44228703f968f008c8c132a9cc36146b5
Felianne Teng <felianneteng@fb.com>
2021-07-13 10:00:23 -0700

    Implement login/logout

--- Locshare/View Controllers/LoginViewController.m
+++ Locshare/View Controllers/LoginViewController.m
@@ -6,8 +6,11 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -15,7 +18,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+}
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (void)loginUser {
+    // Save inputted properties
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    // Check that user is in the Parse database
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+        }
+    }];
+}
+
+- (IBAction)inSignInTap:(id)sender {
+    [self loginUser];
+    
+}
+
+- (IBAction)onRegisterTap:(id)sender {
+    [self registerUser];
 }
 
 /*
--- Locshare/View Controllers/ProfileViewController.m
+++ Locshare/View Controllers/ProfileViewController.m
@@ -6,6 +6,9 @@
 //
 
 #import "ProfileViewController.h"
+#import <Parse/Parse.h>
+#import "SceneDelegate.h"
+#import "LoginViewController.h"
 
 @interface ProfileViewController ()
 
@@ -15,7 +18,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+}
+
+- (IBAction)onLogoutTap:(id)sender {
+    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+        // PFUser.current() will now be nil
+        if (error != nil) {
+            NSLog(@"User log out failed: %@", error.localizedDescription);
+        }
+        else {
+            // After logout, return to login screen
+            NSLog(@"User logged out successfully");
+            SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+            sceneDelegate.window.rootViewController = loginViewController;
+        }
+    }];
 }
 
 /*
